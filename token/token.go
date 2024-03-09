58d319a2d0eb6658a6c38a3262731b3c0e492b68
Foxtrot-14 <noamanahmedai@gmail.com>
2024-03-09 19:00:07 +0530

    initial

---
+++ token/token.go
@@ -0,0 +1,31 @@
+package token
+
+//custom type called TokenType
+type TokenType string
+//struct named token has two fields, Type and Literal
+type Token struct {
+	Type TokenType
+	Literal string
+}
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+	//Identifier
+	IDENT = "IDENT" //name,x,y
+	INT = "INT" //12346
+	//Operators
+	ASSIGN = "="
+	PLUS = "+"
+	//Delimiters
+	COMMA = ","
+	SEMICOLON = ";"
+	//Blocks
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+	//Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
+
