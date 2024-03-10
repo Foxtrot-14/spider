4c15a1a77ccc6a7ab00952040fcf7dca2a8372d9
Foxtrot-14 <noamanahmedai@gmail.com>
2024-03-10 22:37:41 +0530

    read-char-function

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -9,3 +9,15 @@ func New(input string)*Lexer{
 	l := &Lexer{input: input}
 	return l
 }
+/*
+By passing a pointer, you're essentially passing a reference to the original data in memory, rather than making a copy
+*/
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
