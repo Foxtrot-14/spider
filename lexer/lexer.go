21a997b7b3ab0d289d5237f65e9ff84e346375ca
Foxtrot-14 <noamanahmedai@gmail.com>
2024-03-11 22:20:57 +0530

    nextToken

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,4 +1,6 @@
 package lexer
+
+import "go/token"
 type Lexer struct{
 	input        string
 	position     int
@@ -7,6 +9,7 @@ type Lexer struct{
 }
 func New(input string)*Lexer{
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
 }
 /*
@@ -20,4 +23,33 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+func (l *Lexer)NextToken() token.Token{
+	var tok token.Token
+	switch l.ch {
+		case '=':
+			tok = newToken(token.ASSIGN,l.ch)
+		case ';':
+			tok = newToken(token.SEMICOLON,l.ch)
+		case '(':
+			tok = newToken(token.LPAREN,l.ch)
+		case ')':
+			tok = newToken(token.RPAREN,l.ch)
+		case ',':
+			tok = newToken(token.COMMA,l.ch)
+		case '+':
+			tok = newToken(token.PLUS,l.ch)	
+		case '{':
+			tok = newToken(token.LBRACE,l.ch)
+		case '}':
+			tok = newToken(token.RBRACE,l.ch)
+		case 0:
+			tok.Type = token.EOF	 							
+			tok.Literal = ""
+	}
+	l.readChar()
+	return tok
+}
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+return token.Token{Type: tokenType, Literal: string(ch)}
 }
